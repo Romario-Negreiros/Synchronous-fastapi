0a995e838b2537dc86ef75c26d870f8880213a68
Romario Negreiros <nromario482@gmail.com>
2023-12-16 15:30:20 -0300

    refactor: remove mysql asynchronous driver aiomysql and use its default driver

--- core/configs.py
+++ core/configs.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 class Settings(BaseSettings):
     API_V1_URL: str                  = "/api/v1",
-    DB_URL: str                      = f"mysql+aiomysql://{os.getenv('DB_USER')}:{os.getenv('DB_PWD')}@{os.getenv('DB_HOST')}:3360/{os.getenv('DB_NAME')}",
+    DB_URL: str                      = f"mysql://{os.getenv('DB_USER')}:{os.getenv('DB_PWD')}@{os.getenv('DB_HOST')}:3360/{os.getenv('DB_NAME')}",
     DBBaseModel                      = declarative_base()
     
     JWT_TYPE: str                    = "Bearer"
