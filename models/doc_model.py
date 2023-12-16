e5e0fbc576177590b6104c534b987302c0e3b1cd
Romario Negreiros <nromario482@gmail.com>
2023-12-16 20:59:44 -0300

    refactor: use default identation and add id primary key

--- models/doc_model.py
+++ models/doc_model.py
@@ -3,6 +3,9 @@ from sqlalchemy import String, Column, DATETIME
 from core.configs import settings
 
 class DocModel(settings.DBBaseModel):
-    type: Column(String(256))
-    owner: Column(String(256))
-    emissionDate: Column(DATETIME)
+    __tablename__ = "docs"
+    
+    id            = Column(String(256), primary_key=True)
+    type          = Column(String(256))
+    owner         = Column(String(256))
+    emissionDate  = Column(DATETIME)
