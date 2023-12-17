50a8d66bfd9e5702c5686a209f4abbb0cee3ab5d
Romario Negreiros <nromario482@gmail.com>
2023-12-16 21:00:24 -0300

    fix: remove bug comma

--- core/configs.py
+++ core/configs.py
@@ -1,6 +1,6 @@
 from pydantic import BaseSettings
 
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 from dotenv import load_dotenv
 import os
@@ -8,8 +8,8 @@ import os
 load_dotenv()
 
 class Settings(BaseSettings):
-    API_V1_URL: str                  = "/api/v1",
-    DB_URL: str                      = f"mysql://{os.getenv('DB_USER')}:{os.getenv('DB_PWD')}@{os.getenv('DB_HOST')}:3360/{os.getenv('DB_NAME')}",
+    API_V1_URL: str                  = "/api/v1"
+    DB_URL: str                      = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PWD')}@{os.getenv('DB_HOST')}:3306/{os.getenv('DB_NAME')}"
     DBBaseModel                      = declarative_base()
     
     JWT_TYPE: str                    = "Bearer"
