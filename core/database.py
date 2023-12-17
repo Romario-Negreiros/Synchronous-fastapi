b5f9f0b6b8254423beb17f60743922f300bf12f2
Romario Negreiros <nromario482@gmail.com>
2023-12-17 12:23:45 -0300

    refactor: remove typing from session and engine variables

--- core/database.py
+++ core/database.py
@@ -1,11 +1,11 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session as SyncSession
-from sqlalchemy.engine import create_engine, Engine
+from sqlalchemy import create_engine
 from core.configs import settings
 
-engine: Engine = create_engine(settings.DB_URL)
+engine = create_engine(settings.DB_URL)
 
-Session: SyncSession = sessionmaker(
+Session = sessionmaker(
     autocommit       = False,
     autoflush        = False,
     expire_on_commit = False,
