09e6737eb9897a743a2bd21e6b3647c2a5b310b7
Romario Negreiros <nromario482@gmail.com>
2023-12-17 13:01:58 -0300

    refactor: use procedures to query user data from mysql feat: add UserSchemaBase to return type of authenticate function

--- core/auth.py
+++ core/auth.py
@@ -1,47 +1,49 @@
 from typing import Optional
 from datetime import datetime, timezone
 
-from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy.future import select
+from sqlalchemy.orm import Session as SyncSession
 from jose import jwt
 
 from core.configs import settings
-from core.database import Session
-from core.deps import get_session
 from core.security import verify_password
-from models.__all__models import UserModel
+from schemas.user_schemas import UserSchemaBase
 
-oauth2_schema = OAuth2PasswordBearer(tokenUrl = f"{settings.API_V1_URL}/users/login")
+oauth2_schema = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_URL}/users/login")
+
 
 def authenticate(
-        cpf: str, password: str,
-        session: Session = Depends(get_session)
-    ) -> Optional[str]:
-    try:
-        query = select(UserModel).filter(UserModel.cpf == cpf)
-        result = session.execute(query)
-        user = result.scalars().one_or_none()
-        
-        if not user:
+    cpf: str, password: str, session: SyncSession
+) -> Optional[UserSchemaBase]:
+    with session:
+        result = session.execute(
+            "CALL sp_getUserByCpf(:param_cpf)",
+            {"param_cpf": cpf},
+        ).fetchone()
+
+        if not result[0]:
             return None
-        
-        if not verify_password(password, user.password):
+
+        # result = (cpf, name, password)
+        user = {"cpf": result[0], "name": result[1], "password": result[2]}
+
+        if not verify_password(password, user["password"]):
             return None
-        
+
+        del user["password"]
         return user
-    finally:
-        session.close()
+
 
 def generate_jwt(
-        subject: str, user_timezone: Optional[str] = "America/Sao_Paulo"
-    ) -> str:
+    subject: str, user_timezone: Optional[str] = "America/Sao_Paulo"
+) -> str:
     payload = {
         "type": settings.JWT_TYPE,
-        "exp": datetime.now(timezone(user_timezone) + settings.ACCESS_TOKEN_EXPIRE_MINUTES),
+        "exp": datetime.now(
+            timezone(user_timezone) + settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        ),
         "iat": datetime.now(timezone(user_timezone)),
-        "sub": subject
+        "sub": subject,
     }
-    
+
     return jwt.encode(payload, settings.JWT_SECRET, algorithm=settings.ALGORITHM)
-    
