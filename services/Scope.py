186f7030934099cfc80aa620be24217a8f7aed68
RNBSilva <RNBSilva@P381886>
2023-12-18 10:45:29 -0300

    refactor: if clause in verify_scope method

--- services/Scope.py
+++ services/Scope.py
@@ -7,11 +7,9 @@ class ScopeError(Exception):
         super().__init__(message)
 
 class Scope:
-    def verify_scope(self, user: UserSchemaResponse, endpoint_scope: str):
+    def verify_scope(self, user: UserSchemaResponse, endpoint_scope: str) -> None:
         user_scopes: List[str] = user["scopes"].split(",")       
-        if endpoint_scope in user_scopes:
-            return
-        else:
+        if endpoint_scope not in user_scopes:
             raise ScopeError(f"O usuario identificado pelo cpf: {user['cpf']} não tem acesso ao grupo: {endpoint_scope}")
     
 scope = Scope()
