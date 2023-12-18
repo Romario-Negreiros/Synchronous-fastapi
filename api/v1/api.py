65388fc268bae83970a6b2b3f46d8687e717ea67
RNBSilva <RNBSilva@P381886>
2023-12-18 08:42:04 -0300

    feat: include docs router

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from api.v1.endpoints import users
+from api.v1.endpoints import docs
 
 api_router = APIRouter()
 
 api_router.include_router(router=users.router, prefix="/users", tags=["Users"])
+api_router.include_router(router=docs.router, prefix="/docs", tags=["Docs"])
