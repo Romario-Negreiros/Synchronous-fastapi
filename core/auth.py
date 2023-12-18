eae6c8820bf4be869282cca2cb5764dcabda575b
Romario Negreiros <nromario482@gmail.com>
2023-12-17 22:06:41 -0300

    refactor: use timezone function pytz module instead of datetime in generate_jwt refactor: check if result is not null in authenticate

--- core/auth.py
+++ core/auth.py
@@ -1,5 +1,6 @@
 from typing import Optional
-from datetime import datetime, timezone
+from datetime import datetime
+from pytz import timezone
 
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session as SyncSession
@@ -7,21 +8,20 @@ from jose import jwt
 
 from core.configs import settings
 from core.security import verify_password
-from schemas.user_schemas import UserSchemaBase
+from schemas.user_schemas import UserSchemaResponse
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_URL}/users/login")
 
-
 def authenticate(
     cpf: str, password: str, session: SyncSession
-) -> Optional[UserSchemaBase]:
+) -> Optional[UserSchemaResponse]:
     with session:
         result = session.execute(
             "CALL sp_getUserByCpf(:param_cpf)",
             {"param_cpf": cpf},
         ).fetchone()
 
-        if not result[0]:
+        if not result:
             return None
 
         # result = (cpf, name, password)
@@ -37,12 +37,11 @@ def authenticate(
 def generate_jwt(
     subject: str, user_timezone: Optional[str] = "America/Sao_Paulo"
 ) -> str:
+    issued_at = datetime.now(timezone(user_timezone))
     payload = {
         "type": settings.JWT_TYPE,
-        "exp": datetime.now(
-            timezone(user_timezone) + settings.ACCESS_TOKEN_EXPIRE_MINUTES
-        ),
-        "iat": datetime.now(timezone(user_timezone)),
+        "exp": issued_at + settings.ACCESS_TOKEN_EXPIRE_MINUTES,
+        "iat": issued_at,
         "sub": subject,
     }
 
