c841f22ff82b8329e6ec0afacf3f65e73df730d2
RNBSilva <RNBSilva@P381886>
2023-12-18 10:12:54 -0300

    feat: create endpoint to get single doc by its id

--- api/v1/endpoints/docs.py
+++ api/v1/endpoints/docs.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from fastapi import APIRouter, status, Depends, HTTPException, Request
 from sqlalchemy.orm import Session as SyncSession
 
-from schemas.doc_schema import DocSchemaCreate
+from schemas.doc_schema import DocSchema, DocSchemaCreate
 from schemas.user_schemas import UserSchemaResponse
 from core.deps import get_current_user, get_session
 from services.Scope import scope, ScopeError
@@ -36,6 +36,29 @@ def get_docs(
             detail="Não foi possível recuperar os documentos.",
         )
 
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=DocSchema)
+def get_doc(id: int, request: Request, session: SyncSession = Depends(get_session), logged_user: UserSchemaResponse = Depends(get_current_user)):
+    try:
+        scope.verify_scope(user=logged_user, endpoint_scope="docs")
+        with session:
+            result = session.execute("CALL sp_getDocById(:param_id)", {"param_id": id}).fetchone()
+            
+            if not result:
+                return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Documento não encontrado.")
+            
+            # result = (id, type, owner, emissionDate)
+            doc = {"id": result[0], "type": result[1], "owner": result[2], "emissionDate": result[3]}
+            return doc
+    except ScopeError as ex:
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(ex))
+    except Exception as ex:
+        logger.save_log(
+            full_log=str(ex), log_level="ERROR", issued_from=request.url.path
+        )
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Ocorreu um erro ao tentar buscar o documento.",
+        )
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=None)
 def create_doc(
