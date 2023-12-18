dd2b59d6eb46cecf3664d064fffe0e1004139b3a
Romario Negreiros <nromario482@gmail.com>
2023-12-17 22:07:32 -0300

    refactor: import hashing function from hash module instead of CryptoContext

--- core/security.py
+++ core/security.py
@@ -1,9 +1,7 @@
-from passlib.context import CryptContext
-
-CRYPT = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from passlib.hash import pbkdf2_sha256 as crypt
 
 def verify_password(password: str, password_hash: str) -> bool:
-    return CRYPT.verify(password, password_hash)
+    return crypt.verify(password, password_hash)
 
 def generate_password_hash(password: str) -> str:
-    return CRYPT.hash(password)
+    return crypt.hash(password)
