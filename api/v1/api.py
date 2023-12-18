4fef898cc44025530d67a54e926d0d59fd25c39e
RNBSilva <RNBSilva@P381886>
2023-12-18 10:45:12 -0300

    feat: include logs router

--- api/v1/api.py
+++ api/v1/api.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from api.v1.endpoints import users
 from api.v1.endpoints import docs
+from api.v1.endpoints import logs
 
 api_router = APIRouter()
 
 api_router.include_router(router=users.router, prefix="/users", tags=["Users"])
 api_router.include_router(router=docs.router, prefix="/docs", tags=["Docs"])
+api_router.include_router(router=logs.router, prefix="/logs", tags=["Logs"])
