76323b39bc944f7970009cfb14b1efb59a1a7853
Romario Negreiros <nromario482@gmail.com>
2023-12-17 22:59:16 -0300

    refactor: use auto incremental id instead of custom

--- models/log_model.py
+++ models/log_model.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, String, DATETIME
+from sqlalchemy import Column, Integer, String, DATETIME
 
 from core.configs import settings
 
 class LogModel(settings.DBBaseModel):
     __tablename__ = "logs"
     
-    id            = Column(String(256), primary_key=True)
+    id            = Column(Integer, primary_key=True, auto_increment=True)
     full_log      = Column(String(256), nullable=False)
     log_level     = Column(String(256), nullable=False)
     issued_from   = Column(String(256), nullable=False)
