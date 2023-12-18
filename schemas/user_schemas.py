42c000f57caaee7aa1f65f97b7fafdac7db39717
Romario Negreiros <nromario482@gmail.com>
2023-12-17 22:08:14 -0300

    fix: return value in validators if the value is valid refactor: in password validator, use a list to return all validation errors

--- schemas/user_schemas.py
+++ schemas/user_schemas.py
@@ -12,17 +12,25 @@ class UserSchema(BaseModel):
     def validate_cpf(cls, v: str):
         if len(v) != 11:
             raise ValueError("O campo 'cpf' deve conter 11 caracteres.")
+        return v
     
     @validator("password")
     def validate_password(cls, v: str):
+        errors = []
         
         pwd_contain_numbers_regexp = re.compile(r'[0-9]{1,}')
         if not pwd_contain_numbers_regexp.search(v):
-            raise ValueError("A senha deve conter pelo menos um número.")
+            errors.append("A senha deve conter pelo menos um número.")
         
-        pwd_contain_special_characters_regexp = re.compile(r'[&*\s{},=\-().+;\'/]{1,}')
+        pwd_contain_special_characters_regexp = re.compile(r'[&@*\s{},=\-().+;\'/]{1,}')
         if not pwd_contain_special_characters_regexp.search(v):
-            raise ValueError("A senha deve conter pelo menos um caractere especial.")
+            errors.append("A senha deve conter pelo menos um caractere especial.")
+
+        if len(errors) > 0:
+            if len(errors) == 2:
+                raise ValueError(errors[0], errors[1])
+            raise ValueError(errors[0])      
+        return v
         
 class UserSchemaResponse(BaseModel):
     cpf: str
