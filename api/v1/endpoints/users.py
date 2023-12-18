4b5b5a6ac9776d5f88d7d6a19b31f8b2c5047910
Romario Negreiros <nromario482@gmail.com>
2023-12-17 21:49:14 -0300

    feat: add register and get logger user routes refactor: rename /login route function to login

--- api/v1/endpoints/users.py
+++ api/v1/endpoints/users.py
@@ -3,31 +3,76 @@ from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
 
 from sqlalchemy.orm import Session as SyncSession
+from pydantic import ValidationError
 
 from core.configs import settings
 from core.auth import authenticate, generate_jwt
-from core.deps import get_session
+from core.deps import get_session, get_current_user
+from core.security import generate_password_hash
+from schemas.user_schemas import UserSchema, UserSchemaResponse
 
 router = APIRouter()
 
 
+@router.get(
+    "/logged", status_code=status.HTTP_200_OK, response_model=UserSchemaResponse
+)
+def get_logged_user(logged_user=Depends(get_current_user)):
+    return logged_user
+
+
+@router.post(
+    "/register", status_code=status.HTTP_201_CREATED, response_model=UserSchemaResponse
+)
+def register(
+    newUser: UserSchema,
+    session: SyncSession = Depends(get_session),
+):
+    try:
+        with session:
+            session.execute(
+                "CALL sp_setUser(:param_cpf, :param_name, :param_password)",
+                {
+                    "param_cpf": newUser.cpf,
+                    "param_name": newUser.name,
+                    "param_password": generate_password_hash(newUser.password),
+                },
+            )
+            session.commit()
+        return newUser
+    except ValidationError as ex:
+        print(ex)
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Ocorreu um erro ao tentar realizar o registro.",
+        )
+    except Exception as ex:
+        # LOG EXCEPTION
+        print(ex)
+        session.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Ocorreu um erro ao tentar realizar o registro.",
+        )
+
+
 @router.post("/login", status_code=status.HTTP_200_OK)
-def get_user(
+def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
     session: SyncSession = Depends(get_session),
 ):
     try:
-        user = authenticate(
+        user: UserSchemaResponse = authenticate(
             cpf=form_data.username, password=form_data.password, session=session
         )
 
         if not user:
-            raise HTTPException(
+            return JSONResponse(
+                {"detail": "O usuário não foi encontrado."},
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail="O usuário não foi encontrado.",
             )
 
-        access_token: str = generate_jwt(subject=user.cpf)
+        access_token: str = generate_jwt(subject=user["cpf"])
         return JSONResponse(
             {"token_type": settings.JWT_TYPE, "access_token": access_token},
             status_code=status.HTTP_200_OK,
