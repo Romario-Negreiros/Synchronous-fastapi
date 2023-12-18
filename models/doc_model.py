4fc0b938ba59a94850f5ae8159ab58b16b9f7c48
RNBSilva <RNBSilva@P381886>
2023-12-18 07:51:15 -0300

    refactor: make id column an auto-incremental integer

--- models/doc_model.py
+++ models/doc_model.py
@@ -1,11 +1,11 @@
-from sqlalchemy import String, Column, DATETIME
+from sqlalchemy import String, Integer, Column, DATETIME
 
 from core.configs import settings
 
 class DocModel(settings.DBBaseModel):
     __tablename__ = "docs"
     
-    id            = Column(String(256), primary_key=True)
+    id            = Column(Integer, primary_key=True, autoincrement=True)
     type          = Column(String(256))
     owner         = Column(String(256))
     emissionDate  = Column(DATETIME)
